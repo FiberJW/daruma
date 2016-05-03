13f80531b4bb9db692ff2ad7105402f71ff4ba5e
Juwan Wheatley <datwheat@gmail.com>
2016-05-03 07:32:28 -0400

    change new project introduction text

--- actions/new.js
+++ actions/new.js
@@ -46,6 +46,8 @@ module.exports = function(args) {
         '  '
       ) + '\n'
     ).to('./daruma.json');
+    
+    console.log('Installing dependencies...');
 
     shell.exec('npm install -g webpack', {silent:true});
     shell.exec('npm install --save-dev babel-loader babel-core babel-preset-es2015', {silent:true});
@@ -58,6 +60,8 @@ module.exports = function(args) {
       ) + '\n'
     ).to('./daruma.json');
 
+    console.log('Installing dependencies...');
+
     shell.exec('npm install -g babel-cli', {silent:true});
     shell.exec('npm install --save-dev babel-core babel-preset-es2015 babel-preset-stage-0', {silent:true});
   }
@@ -67,7 +71,7 @@ module.exports = function(args) {
     \`cd ${name}/\` to enter folder.
     Write ES2015 code in \`src/\` folder.
     Run \`daruma build\` in root of project directory.
-    Your compiled code is in the \`dist/\` folder.
+    Your compiled code will be in the \`dist/\` folder.
     Enjoy freedom!
   `);
 };
